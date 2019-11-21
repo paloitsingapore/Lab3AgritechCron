99b9f396043a2c1e4454aada293c3e763957fb87
Shobhit Maheshwari <smaheshwari@palo-it.com>
2019-11-21 17:13:41 +0000

    S3Upload

--- LoadDataS3.py
+++ LoadDataS3.py
@@ -1,30 +1,59 @@
 import boto3
 import os
-from zipfile import ZipFile
+import zipfile
 from datetime import datetime
-
+import subprocess 
+from myip import GetIP 
 # Create an S3 client
+print("uploading to cloud on ")
+print (GetIP())
 S3 = boto3.client('s3')
-BUCKET_NAME = 'lab3agritechpaloit'
-log_directory = '/home/pi/logs'
-data_directory ='/home/pi/data'
-zip_log = (datetime.now()+'-'+'logs.zip','w')
-zip_data =(datetime.now()+'-'+'data.zip','w')
+BUCKET_NAME = 'lab3agritechtest'
+log_directory = '/home/pi/logs/'
+data_directory ='/home/pi/data/'
+zip_log = (str(datetime.now())+'-'+'logs.zip')
+zip_data =(str(datetime.now())+'-'+'data.zip')
+zip_log =  zip_log.replace(" ", "_")
+zip_data = zip_data.replace(" ","_")
 
-for file in os.listdir(log_directory):
-    print('in dir',directory)
-    filename = directory + '/' + file
-    print("new filename:\t",filename)
-    if filename.endswith(".log"):
-        DEST='logs/'+file
-        print("destination",DEST)
-        s3.upload_file(filename, BUCKET_NAME, DEST)
-for file in os.listdir(data_directory):
-    print('in dir',directory)
-    filename = directory + '/' + file
-    print("new filename:\t",filename)
-    if filename.endswith(".tar.gz"):
-        DEST='data/'+file
-        print("destination",DEST)
-        s3.upload_file(filename, BUCKET_NAME, DEST)
+with zipfile.ZipFile(log_directory+zip_log, mode='w') as zf:
+    for file in os.listdir(log_directory):
+        filename = log_directory + '/' + file
+        if filename.endswith(".log"):
+            print('Zipping File' + filename)
+            zf.write(filename)
+print("Log Files zipped in "+zip_log)
+
+with zipfile.ZipFile(data_directory+zip_data, mode='w') as zf:
+    for file in os.listdir(data_directory):
+        filename = data_directory + '/' + file
+        if filename.endswith(".tar.gz"):
+            print("zipping file "+filename)
+            zf.write(filename)
+print("data file zipped in "+zip_data)
+
+print("uploading files to s3....")
+S3.upload_file(log_directory+zip_log, BUCKET_NAME, 'logs/'+str(GetIP())+zip_log)
+S3.upload_file(data_directory+zip_data, BUCKET_NAME, 'data/'+str(GetIP())+zip_data)
+print("s3 upload finished")
+
+
+print("Removing Zipped File .....")
+
+log_file  = os.listdir(log_directory)
+data_file = os.listdir(data_directory)
+
+for item in log_file:
+    if item.endswith(".zip"):
+        os.remove( os.path.join( log_directory, item ) )
+
+for item in data_file:
+    if item.endswith(".zip"):
+        os.remove( os.path.join( data_directory, item ) )
+
+print("Zip files removed")
+
+subprocess.call('mv  /home/pi/logs/* /home/pi/backup/logs',shell=True)
+
+subprocess.call('mv /home/pi/data/* /home/pi/backup/data',shell=True)
 
---
+++ myip.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import socket
+import os
+ 
+def GetIP():
+    gw = os.popen("ip -4 route show default").read().split()
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect((gw[2], 0))
+    ipaddr = s.getsockname()[0]
+    print (ipaddr)
+    return ipaddr
