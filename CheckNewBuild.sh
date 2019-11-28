a2153765a0d1790cb2e25793d21afb01b5a4de50
shobhit-palo <smaheshwari@palo-it.com>
2019-11-28 08:43:28 +0000

    Docker and Git Pull Retry

--- CheckNewBuild.sh
+++ CheckNewBuild.sh
@@ -9,10 +9,42 @@ get_latest_version()
 
 pull_docker()
 {	
+       #Docker Image Staus  
         get_latest_version
-	out=$( { sudo docker pull $newversion; } 2>&1 )
-	echo $out
-	#Docker Image Staus
+	out=$(docker pull $newversion 2>docker_pull.txt  | tee; ( exit ${PIPESTATUS[0]})) 
+        pull_exit_code=$?
+        err=$(cat docker_pull.txt)	
+	a=5
+	b=6
+	for (( i=1; i<17; i++ )) 
+	do   
+   		   	if [ $a -gt 60 ]; then
+	     			a=60
+		   	fi   
+
+			if [ $pull_exit_code != 0 ]                        
+			then                                
+				echo Docker Pull Failed......
+				echo Error $err
+				echo -n "Retry $i Wating for $a - min for retry....." 
+			        sleeptime=$((a * 60))	
+				sleep $sleeptime
+ 				get_latest_version
+				out=$(docker pull $newversion 2>docker_pull.txt  | tee; ( exit ${PIPESTATUS[0]}))				
+				pull_exit_code=$? 
+                                echo =================================================================
+				err=$(cat docker_pull.txt)
+				echo $err
+                                echo =================================================================				
+			   	fn=$((a + b)) 
+			   	a=$b 
+			   	b=$fn 
+
+			else 
+                          break 	
+			fi  
+         done
+	echo $out 
 	if [[ $out == *"Image is up to date"* ]]
 	then
 	  echo Image already updated
@@ -21,7 +53,7 @@ pull_docker()
 	  echo  image not found
 	elif [[ $out == *"Downloaded newer image"* ]]
 	then
-	  echo New image downloded
+	  echo New image downloded ,Zipping the image
 	  imagename=$newversion
 	  imagefile=$(echo "$imagename" | sed "s/\//_/g")
 	  sudo docker save $imagename $newversion | gzip > /home/pi/bin/DockerImages/$imagefile.tar.gz
@@ -30,5 +62,11 @@ pull_docker()
 
 #Main Area 
 echo pulling docker
-pull_docker
-
+pidfile=$0.pid
+if [ ! -f ${pidfile} ] ||  ! ps -p `cat ${pidfile}` >/dev/null; then
+ echo $$ >${pidfile}
+ pull_docker
+else
+ echo "Process is already running"
+ exit -1
+fi
--- GitUpdate.sh
+++ GitUpdate.sh
@@ -8,6 +8,16 @@
 #echo $REMOTE
 #echo $BASE
 
- git pull 
+ git pull
+ for((i=1;i<11;i++))
+ do
+   if [ $? != 0 ]; then
+     echo GIT pull failed , Waiting for 5 min for retry $i
+     sleep 300     
+     git pull   
+   else
+     break	   
+   fi 	   
+ done	 
  git diff --name-only HEAD HEAD~ >source_list.txt
  echo source list file update with source to be executed 
