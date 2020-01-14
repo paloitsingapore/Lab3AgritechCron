0447b57740d54e2d6d58c0a7ebe7207693099dc5
shobhit-palo <smaheshwari@palo-it.com>
2020-01-14 08:53:00 +0000

    copy update permission

--- CopyUpdate.sh
+++ CopyUpdate.sh
@@ -9,8 +9,8 @@ do
       scp  /home/pi/Lab3AgritechCron/$line  pi@$var:/home/pi/Lab3AgritechCron/
      fi
     done
+    ssh -l pi $var chmod -R 777 /home/pi/Lab3AgritechCron/.git
     scp -r /home/pi/Lab3AgritechCron/.git pi@$var:/home/pi/Lab3AgritechCron/
-    ssh -l pi $var chmod -R 755 /home/pi/Lab3AgritechCron/.git
     #Send Docker Image to other Nodes
     scp  /home/pi/bin/DockerImages/* pi@$var:/home/pi/bin/DockerImages/
     if [ $? -eq 0 ]; then
@@ -20,7 +20,7 @@ do
     fi	
     echo  Replication Completed ...    
 done
-chmod -R 755 /home/pi/Lab3AgritechCron/.git
+chmod -R 777 /home/pi/Lab3AgritechCron/.git
 echo Deleting Docker image fIles 
 sudo rm -r  /home/pi/bin/DockerImages/*
 echo "" > source_list.txt
