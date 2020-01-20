a6e9b016297bb68245ea449629288918b521c390
shobhit-palo <smaheshwari@palo-it.com>
2020-01-20 06:24:40 +0000

    add retry method for myip function --siming

--- myip.py
+++ myip.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 import socket
 import os
+import time
  
 def GetIP():
-    gw = os.popen("ip -4 route show default").read().split()
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect((gw[2], 0))
-    ipaddr = s.getsockname()[0]
-    return ipaddr
+    for x in range (20):
+        try:
+            gw = os.popen("ip -4 route show default").read().split()
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            s.connect((gw[2], 0))
+            ipaddr = s.getsockname()[0]
+            return ipaddr
+        except:
+            print('Router is not connected. No IP Yet!!!')
+            time.sleep(30)
