70338059518f30f18fdf34966418bb3ecceca837
shobhit-palo <smaheshwari@palo-it.com>
2020-02-03 07:21:21 +0000

    webserver fix

--- switch_off.py
+++ switch_off.py
@@ -2,6 +2,7 @@ import serial
 import json
 import sys
 import datetime
+import time 
 
 import logging
 import logging.handlers
@@ -17,16 +18,39 @@ root.setLevel(os.environ.get("LOGLEVEL", "INFO"))
 root.addHandler(handler)
 
 #SID=sys.argv[1]
+
+def write_file(STATUS):
+    print('inside write file')
+    f = open('webserver_status.txt','w')
+    f.write(str(STATUS))
+    f.close()
+
+def read_file():
+    try:
+        f = open('webserver_status.txt', 'r')
+        status = f.read()
+        f.close
+        print(status)
+        return status
+    except Exception as e:
+        print(e)
+        return 0 
+
 def Switch_Off_Device(SID):
     try:
-        port = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=3.0)
+        if read_file() == '0':
+            write_file(1)
+            time.sleep(3)
+            port = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=3.0)
+            port.close()
+            port.open()
+            port.write(str.encode("ID=" + SID + ",switch off\r\n"))
+        else:
+            logging.error("Too Many Request")
+        write_file(0)
 
-        port.close()
-
-        port.open()
-        port.write(str.encode("ID=" + SID + ",switch off\r\n"))
     except Exception as e:
+        write_file(0)
         logging.error("Not able to switch off the power" + str(e))
         raise
-
 
--- switch_on.py
+++ switch_on.py
@@ -2,7 +2,7 @@ import serial
 import json
 import sys
 import datetime
-
+import time
 import logging
 import logging.handlers
 import os
@@ -18,15 +18,44 @@ root = logging.getLogger()
 root.setLevel(os.environ.get("LOGLEVEL", "INFO"))
 root.addHandler(handler)
 
+def write_file(STATUS):
+    print('inside write file')
+    f = open('webserver_status.txt','w')
+    f.write(str(STATUS))
+    f.close()
+
+def read_file():
+    try:
+        f = open('webserver_status.txt', 'r')
+        status = f.read()
+        f.close
+        print(status)
+        return status
+    except Exception as e:
+        print(e)
+        return 0 
+
 def Switch_On_Device(SID):
     try:
-        port = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=3.0)
+        if read_file() == '0':
+            
+            write_file(1)
+            time.sleep(3)
+            port = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=3.0)
 
-        port.close()
+            port.close()
 
-        port.open()
-        port.write(str.encode("ID=" + SID + ",switch on\r\n"))
+            port.open()
+            port.write(str.encode("ID=" + SID + ",switch on\r\n"))
+
+         
+        else:	
+            logging.error("Too Many Request")
+
+        write_file(0)
+
     except Exception as e:
+        write_file(0)
         logging.error("Not able to switch on the power" + str(e))
         raise
 
