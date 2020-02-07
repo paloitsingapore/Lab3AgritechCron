1a783fcfd67e0bed15abb6c2c4323bef9bb60def
shobhit-palo <smaheshwari@palo-it.com>
2020-02-07 05:33:58 +0000

    siming - adding new brain for the agritech

--- dbHandler.py
+++ dbHandler.py
@@ -32,5 +32,161 @@ def GetSensorType(mac_add):
         return result[0]
     except Exception as e:
         print ("Error: " + e)
+        
+        
+def GetListContainer():
+    query = "select distinct id from containers"
+    try:
+        connection = client.connect("http://localhost:4200", username="crate")
+        cursor = connection.cursor()
+        cursor.execute(query)
+        result = cursor.fetchone()
+        cursor.close()
+        connection.close()
+        return result
+    except Exception as e:
+        print ("Error: " + str(e))
+
+def GetContainerInfo(container_id):
+    query = "SELECT humidity_setup - humidity_range start_point_humid ,  humidity_setup end_point_humid ,temperature_setup + temperature_range  start_point_temp , temperature_setup end_point_temp ,fan fan_switch ,mist mist_switch, fanning,auto_fanning,misting,auto_misting  FROM containers WHERE id = '{}'".format(container_id)     
+    try:
+        connection = client.connect("http://localhost:4200", username="crate")
+        cursor = connection.cursor()
+        cursor.execute(query)
+        result = cursor.fetchone()
+        cursor.close()
+        connection.close()
+        return result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7], result[8], result[9]
+    except Exception as e:
+        print ("Error: " + e)
+
+def GetContainerStatus(container_id, system_type):
+    query = "SELECT {} FROM containers WHERE id = '{}'".format(system_type, container_id)
+    try:
+        connection = client.connect("http://localhost:4200", username="crate")
+        cursor= connection.cursor()
+        cursor.execute(query)
+        result = cursor.fetchone()
+        cursor.close()
+        return result[0]
+    except Exception as e:
+        print ("Error: " + e)
 
         
+    
+def GetAveTempHumid(container_id):
+    query = "select avg(temperature) ,AVG (humidity) from th_data t1 , (select max(time)  time ,mac_Add from th_data  where mac_Add in (select macadd from  sensors where container  in ('{}'))group by mac_Add) t2  where t1.mac_add = t2.mac_Add and t1.time= t2.time;".format(container_id)
+    try:
+        connection = client.connect("http://localhost:4200", username="crate")
+        cursor = connection.cursor()
+        cursor.execute(query)
+        result = cursor.fetchone()
+        cursor.close()
+        connection.close()
+        return result[0], result[1]
+    except Exception as e:
+        print ("Error: " + e)
+        
+
+def UpdateContainerStatus(container_id, system_type, status):
+    query = "update containers set {} = {} where id = '{}'".format(system_type, status, container_id)
+    try:
+        connection = client.connect("http://localhost:4200", username="crate")
+        cursor = connection.cursor()
+        cursor.execute(query)
+        cursor.close()
+        connection.close()
+    except Exception as e:
+        print ("Error: " + e)      
+
+
+def InsertActivity(Aid, sensor_id, timenow, Dtype, avetemp, avehumid):
+    query = "INSERT INTO activities (activity_id, system_id, start_time, system_type, bfr_avg_temp, bfr_avg_humidity) VALUES ('{}','{}','{}','{}','{}','{}')".format(str(Aid), sensor_id, timenow, Dtype, avetemp, avehumid)
+    print(query)
+    try:
+        connection = client.connect("http://localhost:4200", username="crate")
+        cursor = connection.cursor()
+        cursor.execute(query)
+        cursor.close()
+        connection.close()
+    except Exception as e:
+        print ("Error: " + e)
+
+
+def UpdateActivity(activity_id, sensor_id, endtime, avetemp, avehumi):
+    query = "UPDATE activities set end_time = '{}', aftr_avg_temp = '{}', aft_avg_humidity = '{}' where activity_id = '{}' and system_id = '{}'".format(endtime, avetemp, avehumi, activity_id, sensor_id)
+    try:
+        connection = client.connect("http://localhost:4200", username="crate")
+        cursor = connection.cursor()
+        cursor.execute(query)
+        cursor.close()
+        connection.close()
+    except Exception as e:
+        print ("Error: " + e)
+
+
+
+
+def GetSystemStatus(system_type, container_id, status, active):
+    query = "SELECT count(*) FROM user_action WHERE status in {} AND system_type = '{}' AND container_id = '{}' AND activating = '{}'".format(status, system_type, container_id, active)
+    try:
+        connection = client.connect("http://localhost:4200", username="crate")
+        cursor = connection.cursor()
+        cursor.execute(query)
+        result = cursor.fetchone()
+        cursor.close()
+        connection.close()
+        return result[0]
+    except Exception as e:
+        print ("Error: " + e)
+
+
+def UpdateUserActions(status, system_type, activating, container_id, current_status):
+    query = "UPDATE user_action set status = {} where system_type = '{}' and activating = '{}' and container_id = '{}' and status = '{}'".format(status, system_type, activating, container_id, current_status)
+    try:
+        connection = client.connect("http://localhost:4200", username="crate")
+        cursor = connection.cursor()
+        cursor.execute(query)
+        cursor.close()
+        connection.close()
+    except Exception as e:
+        print ("Error: " + e)
+        
+
+def GetDelayTime(status, system_type, container_id, active):
+    query = "SELECT delay FROM user_action WHERE status = {} AND system_type = '{}' AND container_id = '{}' AND activating = '{}'".format(status, system_type, container_id, active)
+    try:
+        connection = client.connect("http://localhost:4200", username="crate")
+        cursor = connection.cursor()
+        cursor.execute(query)
+        result = cursor.fetchone()
+        cursor.close()
+        connection.close()
+        return result[0]
+    except Exception as e:
+        print ("Error: " + e)
+        
+def GetRunningData(status, system_type, container_id):
+    query = "SELECT id, delay, date_format('%Y%m%d%H%i', action_time) FROM user_action WHERE status = '{}' AND system_type = '{}' AND container_id = '{}' LIMIT 1".format(status, system_type, container_id)
+    try:
+        connection = client.connect("http://localhost:4200", username="crate")
+        cursor = connection.cursor()
+        cursor.execute(query)
+        result = cursor.fetchone()
+        cursor.close()
+        connection.close()
+        return result
+    except Exception as e:
+        print ("Error: " + str(e))
+        
+        
+def ClearUserActions(pid, status, remark):
+    query = "UPDATE user_action set status = '{}' , remarks = '{}' where id = '{}'".format(status, remark, pid)
+    try:
+        connection = client.connect("http://localhost:4200", username="crate")
+        cursor = connection.cursor()
+        cursor.execute(query)
+        cursor.close()
+        connection.close()
+    except Exception as e:
+        print ("Error: " + str(e))
---
+++ schedular.py
@@ -0,0 +1,337 @@
+import os
+import sys
+import dbHandler
+import time
+import json
+import requests
+import datetime
+from datetime import timedelta
+
+from multiprocessing import Process
+
+import logging
+import logging.handlers
+import os
+
+handler = logging.handlers.WatchedFileHandler(
+    os.environ.get("LOGFILE", "/home/pi/logs/BRAIN_" + datetime.datetime.today().strftime('%Y-%m-%d') + "_error.log"))
+formatter = logging.Formatter('{asctime} {name} {levelname:8s} {message}',style='{')
+#formatter = logging.Formatter(logging.BASIC_FORMAT)
+handler.setFormatter(formatter)
+root = logging.getLogger()
+root.setLevel(os.environ.get("LOGLEVEL", "INFO"))
+root.addHandler(handler)
+
+switch_server1_ip = "172.16.14.82"
+switch_server2_ip = "172.16.12.203"
+
+def sendhttp_request(sensor_id,status, system):
+    request_str_1 = ''
+    request_str_2 = ''
+    if status == "start":
+        request_str_1 = "http://{}:8090/switch/ON/{}".format(switch_server1_ip, sensor_id)
+        request_str_2 = "http://{}:8090/switch/ON/{}".format(switch_server2_ip, sensor_id)
+    elif status == "stop":
+        request_str_1 = "http://{}:8090/switch/OFF/{}".format(switch_server1_ip, sensor_id)
+        request_str_2 = "http://{}:8090/switch/OFF/{}".format(switch_server2_ip, sensor_id)
+    
+    try:
+        try:
+            response = requests.get(request_str_1)
+            if response.status_code == 200:
+                result = response.json()
+                if result["STATUS"] == 'success':
+                    logging.info("{} SWITCH ID: {} HAS BEEN {}ed".format(system, sensor_id, status))
+                    return True
+                elif result["STATUS"] == 'fail':
+                    logging.info("Server1 {} is not working. Let's change to another Server {}".format(switch_server1_ip, switch_serve2_ip))
+                    response2 = requests.get(request_str_2)
+                    if response2.return_code == 200:
+                        result2 = response2.json()
+                        if result2["STATUS"] == 'success':
+                            logging.info("{} SWITCH ID: {} HAS BEEN {}".format(system, sensor_id, status))
+                            return True
+                        elif result["STATUS"] == 'fail':
+                            logging.error("Server2 {} is not working too. Please check".format(switch_server2_ip))
+                            return False
+                    else:
+                        logging.error("Both servers switch is not working. Please Control the Switch manually")  
+
+        except:
+            logging.info("Server1 {} is not working. Let's change to another Server {}".format(switch_server1_ip, switch_server2_ip))
+            response2 = requests.get(request_str_2)
+            if response2.status_code == 200:
+                result2 = response2.json()
+                if result2["STATUS"] == 'success':
+                    logging.info("{} SWITCH ID: {} HAS BEEN {}ed".format(system, sensor_id, status))
+                    return True
+                elif result["STATUS"] == 'fail':
+                    logging.error("Server2 {} is not working too. Please check".format(switch_server2_ip))
+                    return False
+            else:
+                logging.error("Both servers switch is not working. Please Control the Switch manually")  
+                
+    except Exception as e:
+        logging.error("Both servers switch is not working. Please Control the Switch manually" + str(e))  
+                
+        
+    
+def Timechecking(time_min, container_id, sensor_id, activity_id, typing, systemtype):
+    target_time = datetime.datetime.now() + datetime.timedelta(minutes = time_min)
+    while True:
+        currentmisting = dbHandler.GetContainerStatus(container_id, typing)
+        current_time = datetime.datetime.now()
+        if(not currentmisting):
+            dbHandler.UpdateUserActions('2', systemtype, 'true', container_id, '1')
+            avetemp, avehumid = dbHandler.GetAveTempHumid(container_id)
+            endtime = str(datetime.datetime.now())
+            sendhttp_request(sensor_id, "stop", systemtype)
+            dbHandler.UpdateActivity(activity_id, sensor_id, endtime, avetemp, avehumid)
+            time.sleep(60)
+            break;
+     
+        if current_time > target_time:
+            dbHandler.UpdateUserActions('2', systemtype, 'true', container_id, '1')
+            avetemp, avehumid = dbHandler.GetAveTempHumid(container_id)
+            endtime = str(datetime.datetime.now())
+            sendhttp_request(sensor_id,"stop", systemtype) 
+            dbHandler.UpdateContainerStatus(container_id, typing, "false")
+            dbHandler.UpdateActivity(activity_id, sensor_id, endtime, avetemp, avehumid)
+            time.sleep(60)            
+            break;
+        time.sleep(60)
+	    
+def startmisting(container_id, humid_now, upper_target_humid, lower_target_humid,sensor_id, manual=False, time_duration=0):
+    index = 0
+    indexofstart = True
+    beforehumid=0
+    time_flag = False
+    timenow = str(datetime.datetime.now())
+    dbHandler.UpdateContainerStatus(container_id, "misting", "true")
+    time.sleep(2)
+    
+    while True:
+        avetemp, avehumid = dbHandler.GetAveTempHumid(container_id)
+        currentmisting = dbHandler.GetContainerStatus(container_id, "misting")
+        if indexofstart:
+            dbHandler.InsertActivity(timenow, sensor_id, timenow, "MIST", avetemp, avehumid)
+            indexofstart = False
+
+        if(not currentmisting):
+            endtime = str(datetime.datetime.now())
+            sendhttp_request(sensor_id, "stop", "MIST")
+            #dbHandler.UpdateContainerStatus(container_id, "misting", "false")
+            dbHandler.UpdateActivity(timenow, sensor_id, endtime, avetemp, avehumid)
+            time.sleep(30)
+            break;
+
+        if manual:
+            sendhttp_request(sensor_id,"start", "MIST")
+            p = Process(target=Timechecking, args=(time_duration, container_id, sensor_id, timenow, "misting", "MIST", ))
+            p.start()
+            break;
+            
+        else:
+            if(humid_now < lower_target_humid):
+	        #startmisting-switch keep on starting  
+                sendhttp_request(sensor_id,"start", "MIST")            
+                time.sleep(180)
+            elif(humid_now > lower_target_humid and humid_now < upper_target_humid):
+                time.sleep(60)
+            elif(humid_now > upper_target_humi):
+                endtime = str(datetime.datetime.now())
+                sendhttp_request(sensor_id, "stop", "MIST") 
+                #update database 
+                dbHandler.UpdateContainerStatus(container_id, "misting", "false")
+                dbHandler.UpdateActivity(timenow, sensor_id, endtime, avetemp, avehumid)
+                time.sleep(180)            
+                break;
+            else:
+                print("testing")        
+            
+        if avehumid < beforehumid:
+            print("this is good. humidity is increasing")
+            beforehumid = avehumid
+            time.sleep(100)
+            
+        if beforehumid < avehumid:
+            print("this is the alert. we have problem of switching misting now")
+            ++index
+            if index < 5:
+                print("this is still ok")
+            else:
+                alert("Please check the misting")
+            
+        time.sleep(100)
+        
+            
+def startfanning(container_id, temp_now, upper_target_temp, lower_target_temp,sensor_id, manual=False, time_duration=0):
+    index = 0
+    indexofstart = True
+    beforetemp=100
+    timenow = datetime.datetime.now()
+    dbHandler.UpdateContainerStatus(container_id, "fanning", "true")
+    time.sleep(2)
+    while True:
+        avetemp, avehumid = dbHandler.GetAveTempHumid(container_id)
+        currentfanning = dbHandler.GetContainerStatus(container_id, "fanning")
+        if indexofstart:
+            for each in sensor_id:
+                dbHandler.InsertActivity(int(round(timenow.timestamp())) + int(each), each, str(timenow), "FAN", avetemp, avehumid)
+            indexofstart = False
+            
+        if(not currentfanning):
+            endtime = str(datetime.datetime.now())
+            for each in sensor_id:
+                dbHandler.UpdateActivity(int(round(timenow.timestamp())) + int(each), each, endtime, avetemp, avehumid)
+                sendhttp_request(each, "stop", "FAN")            
+                time.sleep(30)
+            #dbHandler.UpdateContainerStatus(container_id, "fanning", "false")
+            time.sleep(180)
+            break;
+        
+        if manual:
+            for each in sensor_id:
+                sendhttp_request(each, "start", "FAN")
+                p = Process(target=Timechecking, args=(time_duration, container_id, each, int(round(timenow.timestamp())) + int(each), "fanning", "FAN", ))
+                p.start()
+                time.sleep(30)
+            break;
+        
+        else:                 
+            if(temp_now > upper_target_temp):
+	            #startmisting-switch keep on starting  
+                for each in sensor_id:
+                    sendhttp_request(each, "start", "FAN")            
+                    time.sleep(30)
+                time.sleep(180)
+            elif(temp_now < upper_target_temp  and temp_now > lower_target_temp):
+                time.sleep(60)
+            elif(temp_now < lower_target_temp):
+                endtime = str(datetime.datetime.now())
+                for each in sensor_id:
+                    sendhttp_request(each, "stop", "FAN") 
+                    time.sleep(30)
+                #update database 
+                    dbHandler.UpdateActivity(int(round(timenow.timestamp())) + int(each), each, endtime,avetemp, avehumid)
+                dbHandler.UpdateContainerStatus(container_id, "fanning", "false")
+                time.sleep(180)            
+                break;
+            else:
+                print("testing")        
+            
+        if avetemp > beforetemp:
+            print("this is good. Temperature is decreasing")
+            beforetemp = avetemp
+            time.sleep(100)
+        if beforetemp > avetemp:
+            print("this is the alert. we have problem of switching fanninf now")
+            ++index
+            if index < 5:
+                print("this is still ok")
+            else:
+                alert("Please check the fanning")
+            
+        time.sleep(100)
+        
+
+def stopfanning(container_id, fan_id):
+    dbHandler.UpdateContainerStatus(container_id, "fanning", "false") 
+    #for each in fan_id:
+    #    sendhttp_request(each, "stop", "FAN")
+    #    time.sleep(100)
+        
+    time.sleep(180)    
+
+def ShoudStartFanMist(start_humid, start_temp, avetemp, avehumid):
+    fanning = False
+    misting = False
+
+    if(avehumid < start_humid):
+        misting = True
+    if(avetemp > start_temp):
+        fanning = True
+
+    return misting, fanning    
+
+if __name__ == '__main__':
+    ListOfContainer = dbHandler.GetListContainer()
+    for each_container in ListOfContainer:
+        start_humid, stop_humid, start_temp, stop_temp, fan_id, mist_id, fanning, auto_fanning, misting, auto_misting = dbHandler.GetContainerInfo(each_container)
+        avetemp, avehumid = dbHandler.GetAveTempHumid(each_container)
+        should_misting, should_fanning = ShoudStartFanMist(start_humid, start_temp, avetemp, avehumid)
+        fan_id_list = fan_id.split(',')
+        
+        manual_start_misting = dbHandler.GetSystemStatus('MIST', each_container, '(0,1)','true')
+        manual_start_fanning = dbHandler.GetSystemStatus('FAN', each_container, '(0,1)', 'true')
+        
+        manual_stop_misting = dbHandler.GetSystemStatus('MIST', each_container, '(0)', 'false')
+        manual_stop_fanning = dbHandler.GetSystemStatus('FAN', each_container, '(0)', 'false')
+        
+        manual_starting_misting = dbHandler.GetSystemStatus('MIST', each_container, '(0)','true')
+        manual_starting_fanning = dbHandler.GetSystemStatus('FAN', each_container, '(0)', 'true')
+        
+        running_misting = dbHandler.GetRunningData('1','MIST', each_container)
+        running_fanning = dbHandler.GetRunningData('1','FAN', each_container)
+
+        if running_misting:
+            timenow_number = int('{0:%Y%m%d%H%M}'.format(datetime.datetime.now()))
+            time_should = int(running_misting[2]) + int(running_misting[1]) + 10
+            if timenow_number > time_should:
+                dbHandler.ClearUserActions(running_misting[0], '3', 'Unhealthy data')
+                logging.info("Clear Unhealthy Misting Data ID {}".format(running_misting[0]))
+        if running_fanning:
+            timenow_number = int('{0:%Y%m%d%H%M}'.format(datetime.datetime.now()))
+            time_should = int(running_fanning[2]) + int(running_fanning[1]) + 10
+            if timenow_number > time_should:
+                dbHandler.ClearUserActions(running_fanning[0], '3', 'Unhealthy data')
+                logging.info("Clear Unhealthy fanning Data ID {}".format(running_fanning[0]))
+
+        if manual_stop_misting:
+            dbHandler.UpdateUserActions('2', 'MIST', 'false', each_container, '0')
+            dbHandler.UpdateContainerStatus(each_container, "misting", "false")
+            logging.info("Misting System Has Been Deactivated Manually")
+        if manual_stop_fanning:
+            dbHandler.UpdateUserActions('2', 'FAN', 'false', each_container, '0')
+            dbHandler.UpdateContainerStatus(each_container, "fanning", "false")
+            logging.info("Fanning System Has Been Deactivated Manually")
+        if manual_starting_misting:
+            time_delay = dbHandler.GetDelayTime('0', 'MIST', each_container, 'true')
+            dbHandler.UpdateUserActions('1', 'MIST', 'true', each_container, '0')
+            p = Process(target=startmisting, args=(each_container, avehumid, stop_humid, start_humid, mist_id, True, time_delay))
+            p.start()
+            logging.info("Manual is Taking Over And Misting System Has Been Activated Manually For {} Mins.".format(time_delay))
+        
+        if manual_starting_fanning:
+            time_delay = dbHandler.GetDelayTime('0', 'FAN', each_container, 'true')
+            dbHandler.UpdateUserActions('1', 'FAN', 'true', each_container, '0')
+            p = Process(target=startfanning, args=(each_container, avetemp, stop_temp, start_temp, fan_id_list, True, time_delay))
+            p.start()
+            logging.info("Manual is Taking Over And Fanning System Has Been Activated Manually for {} Mins.".format(time_delay))
+        
+        
+        if should_misting and auto_misting and not misting and fanning and not manual_start_misting and not manual_start_fanning:
+            dbHandler.UpdateContainerStatus(each_container, "fanning", "false")
+            time.sleep(20)
+            dbHandler.UpdateContainerStatus(each_container, "misting", "true")
+            time.sleep(300)       
+            p = Process(target=startmisting, args=(each_container, avehumid, stop_humid, start_humid,mist_id, ))
+            p.start()
+            logging.info("Fanning System Has Been Deactivated Automatically AND Misting System Has Been Activated Automatically.")
+        elif should_misting and auto_misting and not misting and not manual_start_misting and not manual_start_fanning:
+            dbHandler.UpdateContainerStatus(each_container, "misting", "true")
+            time.sleep(20)
+            p = Process(target=startmisting, args=(each_container, avehumid, stop_humid, start_humid, mist_id, ))
+            p.start()
+            logging.info("Misting System Has Been Activated Automatically")
+            
+        elif should_fanning and auto_fanning and not fanning and not misting and not manual_start_misting and not manual_start_fanning:
+            dbHandler.UpdateContainerStatus(each_container, "fanning", "true")
+            time.sleep(60)
+            p = Process(target=startfanning, args=(each_container, avetemp, stop_temp, start_temp, fan_id_list, ))
+            p.start()
+            logging.info("Fanning System Has Been Activated Automatically")
+        else:
+            print("No Action is required")
+            logging.info("No Action is required")
+            
