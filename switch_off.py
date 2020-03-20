5f6ea603bc570da6923f864e6b79ff2a9c6da489
Audran <aguiducci@palo-it.com>
2020-03-20 10:53:08 +0800

    q!

--- switch_off.py
+++ switch_off.py
@@ -7,7 +7,7 @@ import time
 import logging
 import logging.handlers
 import os
-
+import persistqueue
 handler = logging.handlers.WatchedFileHandler(
     os.environ.get("LOGFILE", "/home/pi/logs/SWITCH_SENSOR__" + datetime.datetime.today().strftime('%Y-%m-%d') + "_error.log"))
 formatter = logging.Formatter('{asctime} {name} {levelname:8s} {message}',style='{')
@@ -21,11 +21,13 @@ root.addHandler(handler)
 
 def Switch_Off_Device(SID):
     try:
-        port = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=3.0)
-        port.close()
-        port.open()
-        port.write(str.encode("ID=" + SID + ",switch off\r\n"))
-
+        #port = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=3.0)
+        #port.close()
+        #port.open()
+        #port.write(str.encode("ID=" + SID + ",switch off\r\n"))
+        q = persistqueue.SQLiteQueue('run-switch', auto_commit=True)
+        q.put("ID=" +  SID  +",switch off\r\n")
+        logging.info("Switch off command for SID "+ SID +" id added to queue") 
 
     except Exception as e:
         logging.error("Not able to switch off the power" + str(e))
